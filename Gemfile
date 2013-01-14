052f70d4bff8adcce0884eb765953ccafd6523e7
Plugin73 <laplugin73@gmail.com>
2013-01-14 14:32:47 +0400

    add formatter

--- Gemfile
+++ Gemfile
@@ -2,4 +2,7 @@ source "http://rubygems.org"
 
 # Specify your gem's dependencies in configus.gemspec
 gemspec
+
 gem 'coveralls', require: false
+gem 'simplecov', :require => false, :group => :test
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,12 @@
+
 require 'configus'
 require 'bundler/setup'
 
+require 'simplecov'
 require 'coveralls'
-Coveralls.wear!('rails')
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start
